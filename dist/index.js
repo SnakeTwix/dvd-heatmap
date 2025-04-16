e94ad549b006f3ccdf3f8ae1e0647a46e99ad195
SnakeTwix <42946145+SnakeTwix@users.noreply.github.com>
2025-04-16 14:59:38 +0300

    basic stuff

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+// TODO: Check if these are null
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext("2d");
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+// TODO: Check if these are null
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext("2d")!;
